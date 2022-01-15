05416581dff7eeb4e8b39a8a1c596f3b7b3a040c
dloyolar <dieegolr@gmail.com>
2022-01-15 18:18:42 -0300

    feat: add functions to complete or delete todos

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -5,13 +5,16 @@ export const TodoItem = (props) => {
     <li className="TodoItem">
       <span
         className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+        onClick={props.onComplete}
       >
         ✓
       </span>
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete">×</span>
+      <span className="Icon Icon-delete" onClick={props.onDelete}>
+        ×
+      </span>
     </li>
   );
 };
