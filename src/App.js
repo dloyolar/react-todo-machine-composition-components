00ce1c35449fc1f7728c51f9cbd49210fa299ff5
dloyolar <dieegolr@gmail.com>
2022-01-15 18:18:59 -0300

    feat: add core of project, complete, delete and search TODOS

--- src/App.js
+++ src/App.js
@@ -1,30 +1,66 @@
 // import './App.css';
 
+import { useState } from 'react';
 import { CreateTodoButton } from './components/CreateTodoButton/CreateTodoButton';
 import { TodoCounter } from './components/TodoCounter/TodoCounter';
 import { TodoItem } from './components/TodoItem/TodoItem';
 import { TodoList } from './components/TodoList/TodoList';
 import { TodoSearch } from './components/TodoSearch/TodoSearch';
 
-const todos = [
-  { text: 'Cut Onion', completed: false },
-  { text: 'Take react intro course', completed: false },
-  { text: 'Llorar con la llorona', completed: true },
-  { text: 'Ver spiderman de pana', completed: true },
-  { text: 'Llorar con Endgame', completed: false },
+const defaultTodos = [
+  { id: 1, text: 'Cut Onion', completed: false },
+  { id: 2, text: 'Cut Onion', completed: false },
+  { id: 3, text: 'Take react intro course', completed: false },
+  { id: 4, text: 'Llorar con la llorona', completed: true },
+  { id: 5, text: 'Ver spiderman de pana', completed: true },
+  { id: 6, text: 'Llorar con Endgame', completed: false },
 ];
 
 function App() {
+  const [todos, setTodos] = useState(defaultTodos);
+  const [searchValue, setSearchValue] = useState('');
+
+  const completedTodos = todos.filter((todo) => todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter((todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
+  const toggleCompleteTodos = (id) => {
+    const todoIndex = todos.findIndex((todo) => todo.id === id);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (id) => {
+    const todoIndex = todos.findIndex((todo) => todo.id === id);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter completed={completedTodos} total={totalTodos} />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
       <TodoList>
-        {todos.map((todo, i) => (
+        {searchedTodos.map((todo) => (
           <TodoItem
             text={todo.text}
-            key={todo.text + i}
+            key={todo.id}
             completed={todo.completed}
+            onComplete={() => toggleCompleteTodos(todo.id)}
+            onDelete={() => deleteTodo(todo.id)}
           />
         ))}
       </TodoList>
