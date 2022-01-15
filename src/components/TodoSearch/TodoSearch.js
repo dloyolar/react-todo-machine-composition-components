c3cf8f399acf2f0f4f534e4983a9421d03048851
dloyolar <dieegolr@gmail.com>
2022-01-15 18:18:11 -0300

    feat: add search feature

--- src/components/TodoSearch/TodoSearch.js
+++ src/components/TodoSearch/TodoSearch.js
@@ -1,5 +1,17 @@
 import './TodoSearch.css';
 
-export const TodoSearch = () => {
-  return <input placeholder="Search TODOS" className="TodoSearch" />;
+export const TodoSearch = ({ searchValue, setSearchValue }) => {
+  const onSearchValueChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  return (
+    <input
+      type="text"
+      placeholder="Search TODOS"
+      className="TodoSearch"
+      value={searchValue}
+      onChange={onSearchValueChange}
+    />
+  );
 };
