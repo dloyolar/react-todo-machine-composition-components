a92fb541e0dda4a7b9d4edbfaed3ca4b52f48892
dloyolar <dieegolr@gmail.com>
2022-01-15 14:13:29 -0300

    refactor: create css for all components and delete comments

--- src/components/TodoCounter.js
+++
@@ -1,3 +0,0 @@
-export const TodoCounter = () => {
-  return <h2>Completed 2 of 3 TODOs</h2>;
-};
---
+++ src/components/TodoCounter/TodoCounter.js
@@ -0,0 +1,5 @@
+import './TodoCounter.css';
+
+export const TodoCounter = () => {
+  return <h2 className="TodoCounter">Completed 2 of 3 TODOs</h2>;
+};
